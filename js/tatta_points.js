be6f49eb04be61176309a34f4bbda8f42729f9dc
Kang-min Liu <gugod@gugod.org>
2019-06-09 23:08:52 +0900

    temporarily remove unmark these until it is settled.

--- js/tatta_points.js
+++ js/tatta_points.js
@@ -6,7 +6,7 @@ var tatta_points = [
       "coordinates": [124.157277,24.341281]
     },
     "properties": {
-      "name": "石垣ゆいロード店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -16,7 +16,7 @@ var tatta_points = [
       "coordinates": [124.157456,24.338887]
     },
     "properties": {
-      "name": "石垣７３０記念碑前店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -26,7 +26,7 @@ var tatta_points = [
       "coordinates": [124.154708,24.342501]
     },
     "properties": {
-      "name": "石垣市役所前店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -36,7 +36,7 @@ var tatta_points = [
       "coordinates": [124.151043,24.342113]
     },
     "properties": {
-      "name": "石垣浜崎二丁目店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -46,7 +46,7 @@ var tatta_points = [
       "coordinates": [124.160023,24.334996]
     },
     "properties": {
-      "name": "石垣サザンゲートブリッジ店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -56,7 +56,7 @@ var tatta_points = [
       "coordinates": [124.165832,24.337336]
     },
     "properties": {
-      "name": "石垣登野城仲道店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -66,7 +66,7 @@ var tatta_points = [
       "coordinates": [124.16642,24.342216]
     },
     "properties": {
-      "name": "石垣八重高前店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -76,7 +76,7 @@ var tatta_points = [
       "coordinates": [124.164624,24.348878]
     },
     "properties": {
-      "name": "石垣シード店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -86,7 +86,7 @@ var tatta_points = [
       "coordinates": [124.145644,24.349653]
     },
     "properties": {
-      "name": "石垣新栄店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -96,7 +96,7 @@ var tatta_points = [
       "coordinates": [124.16712,24.333199]
     },
     "properties": {
-      "name": "石垣八島店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -106,7 +106,7 @@ var tatta_points = [
       "coordinates": [124.149084,24.352546]
     },
     "properties": {
-      "name": "石垣新川シード店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -116,7 +116,7 @@ var tatta_points = [
       "coordinates": [124.172574,24.341204]
     },
     "properties": {
-      "name": "石垣中央運動公園前店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -126,7 +126,7 @@ var tatta_points = [
       "coordinates": [124.175488,24.340904]
     },
     "properties": {
-      "name": "石垣平得店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -136,7 +136,7 @@ var tatta_points = [
       "coordinates": [124.14096,24.353668]
     },
     "properties": {
-      "name": "石垣真喜良店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -146,7 +146,7 @@ var tatta_points = [
       "coordinates": [124.177432,24.336126]
     },
     "properties": {
-      "name": "石垣真栄里バイパス店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -156,7 +156,7 @@ var tatta_points = [
       "coordinates": [124.18386,24.34348]
     },
     "properties": {
-      "name": "石垣県立八重山病院店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -166,7 +166,7 @@ var tatta_points = [
       "coordinates": [124.185285,24.339117]
     },
     "properties": {
-      "name": "石垣八重山支庁前店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -176,7 +176,7 @@ var tatta_points = [
       "coordinates": [124.193147,24.341295]
     },
     "properties": {
-      "name": "石垣大浜南店"
+      "name": "石垣〇〇"
     }
   },
   {
@@ -186,7 +186,7 @@ var tatta_points = [
       "coordinates": [124.199665,24.354511]
     },
     "properties": {
-      "name": "石垣磯辺店"
+      "name": "石垣〇〇"
     }
   }
 ];
